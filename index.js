deb88658cba6de3a434d59bdd9d5011bb264eaa9
Adithya188288 <adithya.santhosh33@gmail.com>
2019-10-19 10:06:18 +0530

    Added Db - MongoDb - All routes Works Properly

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 const express = require("express")
-const Joi = require("@hapi/joi")
 const genres = require("./routes/genres")
 const morgan = require("morgan")
 const helmet = require("helmet")
+const mongoose = require("mongoose") //Easy to use API to interact with mongoDb
 
 const app = express()
+
+//Db connection
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(res => console.log("Connected To MongoDb"))
+  .catch(err => console.error("Error", err))
 
 //All middlewares
 app.use(helmet())
 //accepts incoming request with json payloads
 app.use(express.json())
 //accepts incoming request with of form containing json payloads
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static("./public")) // any document under public accesible when the server starts
 app.get("env") === "development" && app.use(morgan("tiny")) // al
 app.use("/api/genres", genres)
-console.log(process.env)
+// console.log(process.env)
 
 const port = process.env.PORT || 3000
 
--- routes/genres.js
+++ routes/genres.js
@@ -1,69 +1,70 @@
+const mongoose = require("mongoose")
 const express = require("express")
+const Joi = require("@hapi/joi")
 const router = express.Router()
 
-const genres = [
-  { id: 1, name: "Action" },
-  { id: 2, name: "Drama" },
-  { id: 3, name: "Adventure" }
-]
+//Create a mogoose scheme
+const genreSchema = new mongoose.Schema({
+  name: { type: String, required: true }
+})
+
+//Create a mongoose model
+//Returns a Class which we can use to transact with the Db
+const Genres = mongoose.model("Genre", genreSchema)
 
 //GET request
-
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const genres = await Genres.find()
   res.send(genres)
 })
 
 // Get request - indivudual
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   //check if the value exists
-  const genre = genres.find(g => g.id === Number(req.params.id))
-  if (!genres)
-    return res.status(404).send("The requested resource does not available")
-
+  console.log(req.params.id)
+  const genre = await Genres.findById(req.params.id)
+  if (!genre) {
+    return res.status(404).send("The requested resource is not available")
+  }
   return res.send(genre)
 })
 
 //Post request
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   const { error, value } = validateGenre(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
-  const newGenre = {
-    id: genres.length + 1,
-    name: value.name
-  }
-
-  genres.push(newGenre)
+  let newGenre = new Genres({ name: req.body.name })
+  newGenre = await newGenre.save()
   return res.send(newGenre) // always send the new added obj
 })
 
 //Updating a record
 
-router.put("/:id", (req, res) => {
-  //check if the value exists
-  const genre = genres.find(g => g.id === Number(req.params.id))
-  if (!genre)
-    return res.status(404).send("The requested resource does not available")
-
+router.put("/:id", async (req, res) => {
   const { error, value } = validateGenre(req.body)
   if (error) return res.status(400).send(error.details[0].message)
 
-  genre.name = value.name //updates the value
+  //check if the value exists
+  const genre = await Genres.findByIdAndUpdate(
+    req.params.id,
+    { name: req.body.name },
+    { new: true }
+  )
+  if (!genre)
+    return res.status(404).send("The requested resource is not available")
 
   return res.send(genre) //send the updated value
 })
 
 //Delete request
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   //check if the value exists
-  const genre = genres.find(g => g.id === Number(req.params.id))
+  const genre = await Genres.findByIdAndRemove(req.params.id)
   if (!genre)
-    return res.status(404).send("The requested resource does not available")
-
-  const index = genres.indexOf(genre)
-  genres.splice(index, 1)
+    return res.status(404).send("The requested resource is not available")
 
   return res.send(genre) // return the deleted obj
 })
@@ -71,6 +72,7 @@ router.delete("/:id", (req, res) => {
 // Validating a record
 // Since Joi has been moved to hapi framework. syntax and code has changed check documentation for more deatils
 validateGenre = genre => {
+  console.log("Validating Genre....")
   const schema = Joi.object({
     name: Joi.string().required()
   })
